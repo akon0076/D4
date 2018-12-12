73fa76c5ae92cb221f3608908c5a0d2e8ff057ca
xiangxin <1244254365@qq.com>
2018-12-12 21:16:39 +0800

    修改配置

--- simple/src/main/java/com/cisdi/info/simple/Application.java
+++ simple/src/main/java/com/cisdi/info/simple/Application.java
@@ -1,15 +1,12 @@
 package com.cisdi.info.simple;
 
-import com.cisdi.info.simple.entity.permission.Module;
-import com.cisdi.info.simple.util.ModuleManager;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.domain.EntityScan;
 import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
-import org.springframework.session.data.redis.config.annotation.web.http.EnableRedisHttpSession;
 
-import java.util.Collection;
+
 
 /**
  *
@@ -17,9 +14,8 @@ import java.util.Collection;
  * @date 2018/04/27
  */
 @SpringBootApplication(scanBasePackages = {"com.cisdi.info.simple.*"}, exclude = {SecurityAutoConfiguration.class})
-@EntityScan("com.cisdi.info.*")
+@EntityScan("com.cisdi.info.simple.*")
 @EnableDiscoveryClient(autoRegister = false)
-@EnableRedisHttpSession
 public class Application {
 
 
---
+++ simple/src/main/java/com/cisdi/info/simple/MySQL5DialectUTF8.java
@@ -0,0 +1,12 @@
+package com.cisdi.info.simple;
+
+import org.hibernate.dialect.MySQL5InnoDBDialect;
+import org.springframework.stereotype.Component;
+
+@Component
+public class MySQL5DialectUTF8 extends MySQL5InnoDBDialect {
+    @Override
+    public String getTableTypeString(){
+        return" ENGINE=InnoDB DEFAULT CHARSET=utf8";
+    }
+}
