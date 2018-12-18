a82a27e3a6f203f113bfcacd1eeceea73b1819d4
cbg <1404510094@qq.com>
2018-12-18 16:39:55 +0800

    分配角色增加组织

--- simple/src/main/java/com/cisdi/info/simple/dto/permission/OperatorAndRoleDto.java
+++ simple/src/main/java/com/cisdi/info/simple/dto/permission/OperatorAndRoleDto.java
@@ -6,8 +6,9 @@ package com.cisdi.info.simple.dto.permission;
  */
 public class OperatorAndRoleDto {
     private Long operatorId;
-    private Long[] roleId;
     private Long organizationId;
+    private Long[] addRoles;
+    private Long[] removeRoles;
 
     public Long getOperatorId() {
         return operatorId;
@@ -17,19 +18,27 @@ public class OperatorAndRoleDto {
         this.operatorId = operatorId;
     }
 
-    public Long[] getRoleId() {
-        return roleId;
-    }
-
-    public void setRoleId(Long[] roleId) {
-        this.roleId = roleId;
-    }
-
     public Long getOrganizationId() {
         return organizationId;
     }
 
     public void setOrganizationId(Long organizationId) {
         this.organizationId = organizationId;
+    }
+
+    public Long[] getAddRoles() {
+        return addRoles;
+    }
+
+    public void setAddRoles(Long[] addRoles) {
+        this.addRoles = addRoles;
+    }
+
+    public Long[] getRemoveRoles() {
+        return removeRoles;
+    }
+
+    public void setRemoveRoles(Long[] removeRoles) {
+        this.removeRoles = removeRoles;
     }
 }
