42193f3e862fbacf493f9f66ff486de52f199f3e
chengbg <chengbg1997@qq.com>
2019-04-24 23:12:40 +0800

    passwrod

---
+++ simple/src/main/java/com/cisdi/info/simple/dto/operator/PasswordDto.java
@@ -0,0 +1,59 @@
+package com.cisdi.info.simple.dto.operator;
+
+import java.util.Date;
+
+/**
+ * @author: chengbg
+ * @date: 2019/4/24
+ **/
+public class PasswordDto {
+    private Long operatorId;
+
+    private String pass;
+
+    private String checkPass;
+
+    private Long updateId;
+
+    private Date updateDatetime;
+
+    public String getPass() {
+        return pass;
+    }
+
+    public void setPass(String pass) {
+        this.pass = pass;
+    }
+
+    public String getCheckPass() {
+        return checkPass;
+    }
+
+    public void setCheckPass(String checkPass) {
+        this.checkPass = checkPass;
+    }
+
+    public Long getOperatorId() {
+        return operatorId;
+    }
+
+    public void setOperatorId(Long operatorId) {
+        this.operatorId = operatorId;
+    }
+
+    public Long getUpdateId() {
+        return updateId;
+    }
+
+    public void setUpdateId(Long updateId) {
+        this.updateId = updateId;
+    }
+
+    public Date getUpdateDatetime() {
+        return updateDatetime;
+    }
+
+    public void setUpdateDatetime(Date updateDatetime) {
+        this.updateDatetime = updateDatetime;
+    }
+}
